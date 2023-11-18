0bb2aecaaa5cb4f19d327cc040de2e87719a0c34
Joshua Johnson <99578151+joshuaj03@users.noreply.github.com>
2023-11-18 15:48:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var a;
+a = document.getElementById("colorpicker").value;
+console.log(a);
+
+function getColor() {
+    var colorpicker = document.getElementById("colorpicker");
+    var selectedColor = colorpicker.value;
+    console.log(selectedColor);
+}
